f1724ec696788c545609a03cbda86b4a86e734db
Carlos Agullo <carlos.agullo@gmail.com>
2019-04-26 20:08:56 +0200

    Add string with two numbers

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -1,8 +1,18 @@
 describe 'String Addition' do
   describe '.add' do
-    context 'given an empty string' do
+    context 'given no input' do
       it 'returns 0' do
         expect(add()).to eql(0)
+      end
+    end
+    context 'given an empty string' do
+      it 'returns 0' do
+        expect(add('')).to eql(0)
+      end
+    end
+    context 'given the string "1,2"' do
+      it 'returns 3' do
+        expect(add('1,2')).to eql(3)
       end
     end
   end
--- string_addition.rb
+++ string_addition.rb
@@ -1,3 +1,8 @@
-def add(input_string = nil)
-  0
+def add(input_string = '')
+  input_values = string_to_numbers(input_string)
+  input_values.reduce(0) { |sum, current_value| sum + current_value }
+end
+
+def string_to_numbers(input_string, separator = ',')
+  input_string.split(separator).map(&:to_i)
 end
