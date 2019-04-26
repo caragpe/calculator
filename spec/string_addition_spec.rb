363e0846883a3bd14108b1f0520bb24ca5342f87
Carlos Agullo <carlos.agullo@gmail.com>
2019-04-27 00:38:07 +0200

    Add support to newline characters as part of the string

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -7,17 +7,28 @@ describe 'String Addition' do
     end
     context 'given an empty string' do
       it 'returns 0' do
-        expect(add('')).to eql(0)
+        expect(add("")).to eql(0)
       end
     end
     context 'given the string "1,2"' do
       it 'returns 3' do
-        expect(add('1,2')).to eql(3)
+        expect(add("1,2")).to eql(3)
       end
     end
     context 'given the string "1,2,3,4,5"' do
       it 'returns 15' do
-        expect(add('1,2,3,4,5')).to eql(15)
+        expect(add("1,2,3,4,5")).to eql(15)
+      end
+    end
+    context 'given the input string includes "\n" character(s)' do
+      it 'returns 6 when input is "1,\n2,3"' do
+        expect(add("1,\n2,3")).to eql(6)
+      end
+      it 'returns 7 when input is "1\n,2,4"' do
+        expect(add("1,\n,2,4")).to eql(7)
+      end
+      it 'returns 8 when input is "2\n,\n2,4\n"' do
+        expect(add("2\n,\n2,4\n")).to eql(8)
       end
     end
   end
--- string_addition.rb
+++ string_addition.rb
@@ -1,8 +1,18 @@
-def add(input_string = '')
+def add(input_string = "")
   input_values = string_to_numbers(input_string)
+  sanitize_newlines(input_string) if has_newline?(input_string)
   input_values.reduce(0) { |sum, current_value| sum + current_value }
 end
 
 def string_to_numbers(input_string, separator = ',')
   input_string.split(separator).map(&:to_i)
+end
+
+def has_newline?(input_string)
+  new_line_regex = Regexp.new("\n")
+  !input_string.match(new_line_regex).nil?
+end
+
+def sanitize_newlines(input_string)
+  input_string.gsub!("\n","")
 end
