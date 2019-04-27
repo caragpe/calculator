4709daae9bba028983188c71598cd8b3aa395841
Carlos Agullo <carlos.agullo@gmail.com>
2019-04-27 13:52:54 +0200

    Add main.rb for demoing solution

---
+++ main.rb
@@ -0,0 +1,17 @@
+require './string_addition'
+
+myStringNumberAddition = StringAddition.new()
+
+string_array = [
+  "",
+  "1,2",
+  "1\n,2,3",
+  "//@@\n2@@2@@1",
+  "//$$$,**,^\n1$$$2**3^5**6"
+]
+string_array.each do |string|
+  puts "Input string: #{string}"
+  puts "Return value: #{myStringNumberAddition.add(string)}"
+  puts "Return value type is: #{myStringNumberAddition.add(string).class}"
+  puts "---------"
+end
