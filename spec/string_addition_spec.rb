3ffcbc7e5cd124032884d4687758c50c5af183b2
Carlos Agullo <carlos.agullo@gmail.com>
2019-04-27 13:39:27 +0200

    Move methods to a class. Set .add as unique public method. Refactor

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -1,83 +1,86 @@
 describe 'String Addition' do
+  before(:each) do
+    @myAddition = StringAddition.new()
+  end
   describe '.add' do
     context 'given no input' do
       it 'returns 0' do
-        expect(add()).to eql(0)
+        expect(@myAddition.add()).to eql(0)
       end
     end
     context 'given an empty string' do
       it 'returns 0' do
-        expect(add("")).to eql(0)
+        expect(@myAddition.add("")).to eql(0)
       end
     end
     context 'given the string "1,2"' do
       it 'returns 3' do
-        expect(add("1,2")).to eql(3)
+        expect(@myAddition.add("1,2")).to eql(3)
       end
     end
     context 'given the string "1,2,5"' do
       it 'returns 8' do
-        expect(add("1,2,5")).to eql(8)
+        expect(@myAddition.add("1,2,5")).to eql(8)
       end
     end
     context 'given the input string includes "\n" character(s)' do
       it 'returns 6 when input is "1\n,2,3"' do
-        expect(add("1\n,2,3")).to eql(6)
+        expect(@myAddition.add("1\n,2,3")).to eql(6)
       end
       it 'returns 7 when input is "1\n,2,4"' do
-        expect(add("1,\n,2,4")).to eql(7)
+        expect(@myAddition.add("1,\n,2,4")).to eql(7)
       end
       it 'returns 8 when input is "2\n,\n2,4\n"' do
-        expect(add("2\n,\n2,4\n")).to eql(8)
+        expect(@myAddition.add("2\n,\n2,4\n")).to eql(8)
       end
     end
     context 'given the string contains values larger than 1000"' do
       it 'ignores values large than 1000 with an input "2,1001"' do
-        expect(add("2,1001")).to eql(2)
+        expect(@myAddition.add("2,1001")).to eql(2)
       end
     end
     context 'given the input string includes any single-character as delimiter instead of ","' do
       it 'returns 8 when input is "//;\n1;3;4"' do
-        expect(add("//;\n1;3;4")).to eql(8)
+        expect(@myAddition.add("//;\n1;3;4")).to eql(8)
       end
       it 'returns 8 when input is "//$\n1$2$3"' do
-        expect(add("//$\n1$2$3")).to eql(6)
+        expect(@myAddition.add("//$\n1$2$3")).to eql(6)
       end
       it 'returns 7 when input is "//@\n2@2@1\n@2"' do
-        expect(add("//@\n2@2@1\n@2")).to eql(7)
+        expect(@myAddition.add("//@\n2@2@1\n@2")).to eql(7)
       end
       it 'returns 13 when input is "//@\n2@3@8"' do
-        expect(add("//@\n2@3@8")).to eql(13)
+        expect(@myAddition.add("//@\n2@3@8")).to eql(13)
       end
     end
     context 'given the input string includes a multi-character as delimiter instead of ","' do
       it 'returns 5 when input is "//@@\n2@@2@@1"' do
-        expect(add("//@@\n2@@2@@1")).to eql(5)
+        expect(@myAddition.add("//@@\n2@@2@@1")).to eql(5)
       end
       it 'returns 7 when input is "//***\n2***2***1\n***2"' do
-        expect(add("//***\n2***2***1\n***2")).to eql(7)
+        expect(@myAddition.add("//***\n2***2***1\n***2")).to eql(7)
       end
     end
     context 'given the input string includes a negative number' do
       it 'throws an expection' do
-        expect{ add("1,-2") }.to raise_error(RuntimeError)
+        expect{ @myAddition.add("1,-2") }.to raise_error(RuntimeError)
       end
       it 'displays error message "Negatives not allowed"' do
-        expect{ add("1,-2") }.to raise_error(RuntimeError, /Negatives not allowed/)
+        expect{ @myAddition.add("1,-2") }.to raise_error(RuntimeError, /Negatives not allowed/)
       end
       it 'displays invalid values' do
-        expect{ add("1,-2,-5") }.to raise_error(RuntimeError, /\[-2, -5\]/)
+        expect{ @myAddition.add("1,-2,-5") }.to raise_error(RuntimeError, /\[-2, -5\]/)
       end
     end
     context 'given the input string includes more that one custom value delimiter' do
       it 'returns 6 when the input string is "//$,@\n1$2@3"' do
-        expect(add("//$,@\n1$2@3")).to eql(6)
+        expect(@myAddition.add("//$,@\n1$2@3")).to eql(6)
       end
       it 'returns 11 when the input string is "//$,*,^\n1$2*3^5"' do
-        expect(add("//$,*,^\n1$2*3^5")).to eql(11)
+        expect(@myAddition.add("//$,*,^\n1$2*3^5")).to eql(11)
       end
       it 'returns 11 when the input string is "//$$$,**,^\n1$$$2**3^5**6"' do
-        expect(add("//$$$,**,^\n1$$$2**3^5**6")).to eql(17)
+        expect(@myAddition.add("//$$$,**,^\n1$$$2**3^5**6")).to eql(17)
       end
     end
   end
--- string_addition.rb
+++ string_addition.rb
@@ -1,54 +1,59 @@
 require 'byebug'
 
-def add(input_string = "")
-  separator = custom_delimiter(input_string) || ","
-  input_values = string_to_array(input_string, separator)
-  return if error_when_negative_value(input_values)
-  sanitize_newlines(input_string) if has_newline?(input_string)
-  input_values.reduce(0) { |sum, current_value| sum + sanitize_values_larger_than_1000(current_value)}
-end
+class StringAddition
+  attr_reader :string_separator, :input_values
 
-def string_to_array(input_string, separator)
-  input_string.split(separator).map(&:to_i)
-end
+  def add(input_string = "")
+    @string_separator = custom_delimiter(input_string) || ","
+    @input_values = string_to_array(input_string)
+    remove_newline_characters(input_string) if has_newline?(input_string)
+    return if error_when_negative_value
+    calculate_sum
+  end
 
-def has_newline?(input_string)
-  new_line_regex = Regexp.new("\n")
-  input_string.match(new_line_regex).nil? ? false : true
-end
+  private
+    def string_to_array(input_string)
+      input_string.split(@string_separator).map(&:to_i)
+    end
 
-def sanitize_newlines(input_string)
-  input_string.gsub!("\n","")
-end
+    def has_newline?(input_string)
+      new_line_regex = Regexp.new("\n")
+      input_string.match(new_line_regex).nil? ? false : true
+    end
 
-def custom_delimiter(input_string)
-  delimiter_control_characters = Regexp.new("//")
-  match_result = input_string.match(delimiter_control_characters)
-  match_result.nil? ? nil : extract_delimiter_characters(input_string)
-end
+    def remove_newline_characters(input_string)
+      input_string.gsub!("\n","")
+    end
 
-def extract_delimiter_characters(input_string)
-  delimiter_candidate = input_string.split("\n")[0].gsub("//","").split(",")
-  regex_delimiter(delimiter_candidate)
-end
+    def error_when_negative_value
+      negative_values = []
+      @input_values.map{ |value| negative_values << value if value < 0 }
+      raise "Negatives not allowed: #{negative_values.inspect}" if negative_values.size > 0
+      false
+    end
 
-def regex_delimiter(delimiters_array)
-  escaped_delimiter_array = delimiters_array.map { |delimiter| Regexp.escape(delimiter) }
-  escaped_delimiter_string = escaped_delimiter_array.join("|")
-  Regexp.new(escaped_delimiter_string)
-end
+    def custom_delimiter(input_string)
+      delimiter_control_characters = Regexp.new("//")
+      match_result = input_string.match(delimiter_control_characters)
+      match_result.nil? ? nil : extract_delimiter_characters(input_string)
+    end
 
-def error_when_negative_value(values_array)
-  negative_values = []
-  values_array.map{ |value| negative_values << value if is_negative?(value) }
-  raise "Negatives not allowed: #{negative_values.inspect}" if negative_values.size > 0
-  false
-end
+    def extract_delimiter_characters(input_string)
+      delimiter_candidate = input_string.split("\n")[0].gsub("//","").split(",")
+      regex_delimiter(delimiter_candidate)
+    end
 
-def sanitize_values_larger_than_1000(value)
-  value <= 1000 ? value : 0
-end
+    def regex_delimiter(delimiters_array)
+      escaped_delimiter_array = delimiters_array.map { |delimiter| Regexp.escape(delimiter) }
+      escaped_delimiter_string = escaped_delimiter_array.join("|")
+      Regexp.new(escaped_delimiter_string)
+    end
 
-def is_negative?(x)
-  x < 0
+    def calculate_sum
+      @input_values.reduce(0) { |sum, current_value| sum + sanitize_values_larger_than_1000(current_value)}
+    end
+
+    def sanitize_values_larger_than_1000(value)
+      value <= 1000 ? value : 0
+    end
 end
