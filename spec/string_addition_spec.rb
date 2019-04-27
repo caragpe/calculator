a20534f9a482c400e94fca7ba71b26efd26df8a2
Carlos Agullo <carlos.agullo@gmail.com>
2019-04-27 02:23:30 +0200

    Add expection support. Also small fixes in old tests.

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -15,20 +15,53 @@ describe 'String Addition' do
         expect(add("1,2")).to eql(3)
       end
     end
-    context 'given the string "1,2,3,4,5"' do
-      it 'returns 15' do
-        expect(add("1,2,3,4,5")).to eql(15)
+    context 'given the string "1,2,5"' do
+      it 'returns 8' do
+        expect(add("1,2,5")).to eql(8)
       end
     end
     context 'given the input string includes "\n" character(s)' do
-      it 'returns 6 when input is "1,\n2,3"' do
-        expect(add("1,\n2,3")).to eql(6)
+      it 'returns 6 when input is "1\n,2,3"' do
+        expect(add("1\n,2,3")).to eql(6)
       end
       it 'returns 7 when input is "1\n,2,4"' do
         expect(add("1,\n,2,4")).to eql(7)
       end
       it 'returns 8 when input is "2\n,\n2,4\n"' do
         expect(add("2\n,\n2,4\n")).to eql(8)
+      end
+    end
+    context 'given the input string includes any single-character as delimiter instead of ","' do
+      it 'returns 8 when input is "//;\n1;3;4"' do
+        expect(add("//;\n1;3;4")).to eql(8)
+      end
+      it 'returns 8 when input is "//$\n1$2$3"' do
+        expect(add("//$\n1$2$3")).to eql(6)
+      end
+      it 'returns 7 when input is "//@\n2@2@1\n@2"' do
+        expect(add("//@\n2@2@1\n@2")).to eql(7)
+      end
+      it 'returns 13 when input is "//@\n2@3@8"' do
+        expect(add("//@\n2@3@8")).to eql(13)
+      end
+    end
+    context 'given the input string includes a multi-character as delimiter instead of ","' do
+      it 'returns 5 when input is "//@@\n2@@2@@1"' do
+        expect(add("//@@\n2@@2@@1")).to eql(5)
+      end
+      it 'returns 7 when input is "//***\n2***2***1\n***2"' do
+        expect(add("//***\n2***2***1\n***2")).to eql(7)
+      end
+    end
+    context 'given the input string includes a negative number' do
+      it 'throws an expection' do
+        expect{ add("1,-2") }.to raise_error(RuntimeError)
+      end
+      it 'throws an expection with error msg "Negatives not allowed"' do
+        expect{ add("1,-2") }.to raise_error(RuntimeError, /Negatives not allowed/)
+      end
+      it 'throws an expection displaying invalid values' do
+        expect{ add("1,-2,-5") }.to raise_error(RuntimeError, /\[-2, -5\]/)
       end
     end
   end
--- string_addition.rb
+++ string_addition.rb
@@ -1,18 +1,38 @@
 def add(input_string = "")
-  input_values = string_to_numbers(input_string)
+  separator = custom_delimiter(input_string) || ","
+  input_values = string_to_array(input_string, separator)
+  return if error_when_negative_value(input_values)
   sanitize_newlines(input_string) if has_newline?(input_string)
   input_values.reduce(0) { |sum, current_value| sum + current_value }
 end
 
-def string_to_numbers(input_string, separator = ',')
+def string_to_array(input_string, separator)
   input_string.split(separator).map(&:to_i)
 end
 
 def has_newline?(input_string)
   new_line_regex = Regexp.new("\n")
-  !input_string.match(new_line_regex).nil?
+  input_string.match(new_line_regex).nil? ? false : true
 end
 
 def sanitize_newlines(input_string)
   input_string.gsub!("\n","")
+end
+
+def custom_delimiter(input_string)
+  new_line = "\n"
+  new_delimiter_regex = Regexp.new("//")
+  match_result = input_string.match(new_delimiter_regex)
+  match_result.nil? ? nil : input_string.split(new_line)[0].gsub("//","")
+end
+
+def error_when_negative_value(values_array)
+  negative_values = []
+  values_array.map{ |value| negative_values << value if is_negative?(value) }
+  raise "Negatives not allowed: #{negative_values.inspect}" if negative_values.size > 0
+  false
+end
+
+def is_negative?(x)
+  x < 0
 end
